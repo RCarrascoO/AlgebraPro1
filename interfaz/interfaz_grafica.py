0e4ceb839c43b10129aa748fba02d2eb0edf41e9
Rodrigo Carmonaa <rcarmona2023@alu.uct.cl>
2025-09-09 17:02:07 -0300

    a

--- interfaz/interfaz_grafica.py
+++ interfaz/interfaz_grafica.py
@@ -11,3 +11,18 @@ class InterfazGrafica:
         self.func_str = tk.StringVar()
         self.x_value = tk.DoubleVar()
         self.create_widgets()
+
+    def create_widgets(self):
+        tk.Label(self.root, text="ingrese la función (f(x))").pack()
+        tk.Entry(self.root, textvariable=self.func_str, width=50 ).pack()
+        tk.Label(self.root, text="ingrese el valor de x").pack()
+        tk.Entry(self.root,textvariable=self.x_value, width=50).pack()
+        tk.Button(self.root, text="analizar función", command=self.analizar_funcion).pack()
+        tk.Button(self.root, text="graficar función", command=self.grafciar_funcion).pack()
+        
+
+
+
+root = tk.Tk()
+app = InterfazGrafica(root)
+root.mainloop()
