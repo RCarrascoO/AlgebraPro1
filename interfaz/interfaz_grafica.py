32b0eb5c628edbedec4a9ecb7f2332dd71491352
Rodrigo Carmonaa <rcarmona2023@alu.uct.cl>
2025-09-09 16:29:24 -0300

    class

--- interfaz/interfaz_grafica.py
+++ interfaz/interfaz_grafica.py
@@ -3,3 +3,11 @@ from tkinter import messagebox
 import sympy as sp
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class InterfazGrafica:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Calculadora Simbólica")
+        self.func_str = tk.StringVar()
+        self.x_value = tk.DoubleVar()
+        self.create_widgets()
